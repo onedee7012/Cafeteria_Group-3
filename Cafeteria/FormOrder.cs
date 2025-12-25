b56390ebaf7349e5f939bc3f4836eac869aa2b89
minhthu0605 <ngothiminhthu0806@gmail.com>
2025-12-25 23:14:04 +0700

    Thêm chức năng giảm giá và tính tiền trả lại khách

--- Cafeteria/FormOrder.cs
+++ Cafeteria/FormOrder.cs
@@ -16,5 +16,31 @@ namespace Cafeteria
         {
             InitializeComponent();
         }
+    private void tbdiscount_TextChanged(object sender, EventArgs e)
+        {
+            if (float.TryParse(tbtotal.Text, out float total) && int.TryParse(tbdiscount.Text, out int discount))
+            {
+                float finalPrice = total - (total * discount / 100f);
+                tbprice.Text = finalPrice.ToString("0.##");
+            }
+            else
+            {
+                tbprice.Text = tbtotal.Text;
+            }
+        }
+
+        private void tbamount_TextChanged(object sender, EventArgs e)
+        {
+            if (float.TryParse(tbamount.Text, out float amount) && float.TryParse(tbprice.Text, out float price))
+            {
+                float change = amount - price;
+                tbchange.Text = change.ToString("0.##");
+            }
+            else
+            {
+                tbchange.Text = "";
+            }
+        }
     }
 }
+
