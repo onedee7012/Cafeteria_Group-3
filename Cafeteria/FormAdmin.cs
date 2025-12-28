95296352ace9eacbfdc00aa45b524d354c3b2451
onedee7012 <filmholic7012@gmail.com>
2025-12-28 08:29:09 +0700

    Update FormAdmin.cs

--- Cafeteria/FormAdmin.cs
+++ Cafeteria/FormAdmin.cs
@@ -1,12 +1,18 @@
-﻿using System;
+using Cafeteria.DAL;
+using Cafeteria.DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Menu = Cafeteria.DTO.Menu;
 
 namespace Cafeteria
 {
