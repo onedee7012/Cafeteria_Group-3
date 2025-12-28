49c0b1b81171bc74fce3c21e662d4e6ca1f7e23b
Binh <filmholic7012@gmail.com>
2025-12-28 08:35:41 +0700

    Update FormHomeUI

--- Cafeteria/FormHome.Designer.cs
+++ Cafeteria/FormHome.Designer.cs
@@ -28,13 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHome));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.orderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.accountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.membershipToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tbfn_home = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.tbun_home = new System.Windows.Forms.TextBox();
@@ -44,10 +43,7 @@
             this.ptbava = new System.Windows.Forms.PictureBox();
             this.btlogout = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.membershipToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ptbava)).BeginInit();
             this.panel1.SuspendLayout();
@@ -56,7 +52,6 @@
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.Color.SeaGreen;
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.adminToolStripMenuItem,
@@ -65,8 +60,8 @@
             this.membershipToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
-            this.menuStrip1.Size = new System.Drawing.Size(1245, 60);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(830, 39);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -75,7 +70,7 @@
             this.adminToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.adminToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
-            this.adminToolStripMenuItem.Size = new System.Drawing.Size(158, 54);
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(100, 35);
             this.adminToolStripMenuItem.Text = "Admin";
             this.adminToolStripMenuItem.Click += new System.EventHandler(this.adminToolStripMenuItem_Click);
             // 
@@ -84,7 +79,7 @@
             this.orderToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.orderToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.orderToolStripMenuItem.Name = "orderToolStripMenuItem";
-            this.orderToolStripMenuItem.Size = new System.Drawing.Size(143, 54);
+            this.orderToolStripMenuItem.Size = new System.Drawing.Size(89, 35);
             this.orderToolStripMenuItem.Text = "Order";
             this.orderToolStripMenuItem.Click += new System.EventHandler(this.orderToolStripMenuItem_Click);
             // 
@@ -93,32 +88,28 @@
             this.accountToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.accountToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.accountToolStripMenuItem.Name = "accountToolStripMenuItem";
-            this.accountToolStripMenuItem.Size = new System.Drawing.Size(185, 54);
+            this.accountToolStripMenuItem.Size = new System.Drawing.Size(117, 35);
             this.accountToolStripMenuItem.Text = "Account";
             this.accountToolStripMenuItem.Click += new System.EventHandler(this.accountToolStripMenuItem_Click);
+            // 
+            // membershipToolStripMenuItem
+            // 
+            this.membershipToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 13.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.membershipToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.membershipToolStripMenuItem.Name = "membershipToolStripMenuItem";
+            this.membershipToolStripMenuItem.Size = new System.Drawing.Size(158, 35);
+            this.membershipToolStripMenuItem.Text = "Membership";
+            this.membershipToolStripMenuItem.Click += new System.EventHandler(this.membershipToolStripMenuItem_Click);
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1245, 948);
+            this.panel2.Size = new System.Drawing.Size(830, 607);
             this.panel2.TabIndex = 3;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(449, -281);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(842, 1250);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
             // 
             // tbfn_home
             // 
@@ -126,11 +117,10 @@
             this.tbfn_home.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tbfn_home.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbfn_home.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.tbfn_home.Location = new System.Drawing.Point(22, 375);
-            this.tbfn_home.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbfn_home.Location = new System.Drawing.Point(15, 240);
             this.tbfn_home.Name = "tbfn_home";
             this.tbfn_home.ReadOnly = true;
-            this.tbfn_home.Size = new System.Drawing.Size(506, 64);
+            this.tbfn_home.Size = new System.Drawing.Size(337, 40);
             this.tbfn_home.TabIndex = 1;
             this.tbfn_home.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -139,10 +129,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(14, 492);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(9, 315);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(201, 50);
+            this.label1.Size = new System.Drawing.Size(124, 31);
             this.label1.TabIndex = 2;
             this.label1.Text = "Username:";
             // 
@@ -152,11 +141,10 @@
             this.tbun_home.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tbun_home.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbun_home.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.tbun_home.Location = new System.Drawing.Point(208, 484);
-            this.tbun_home.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbun_home.Location = new System.Drawing.Point(139, 310);
             this.tbun_home.Name = "tbun_home";
             this.tbun_home.ReadOnly = true;
-            this.tbun_home.Size = new System.Drawing.Size(320, 58);
+            this.tbun_home.Size = new System.Drawing.Size(213, 36);
             this.tbun_home.TabIndex = 3;
             this.tbun_home.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -165,10 +153,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold);
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(102, 570);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(68, 365);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 50);
+            this.label2.Size = new System.Drawing.Size(65, 31);
             this.label2.TabIndex = 4;
             this.label2.Text = "Role:";
             // 
@@ -178,11 +165,10 @@
             this.tbr_home.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tbr_home.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbr_home.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.tbr_home.Location = new System.Drawing.Point(208, 562);
-            this.tbr_home.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbr_home.Location = new System.Drawing.Point(139, 360);
             this.tbr_home.Name = "tbr_home";
             this.tbr_home.ReadOnly = true;
-            this.tbr_home.Size = new System.Drawing.Size(320, 58);
+            this.tbr_home.Size = new System.Drawing.Size(213, 36);
             this.tbr_home.TabIndex = 5;
             this.tbr_home.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -191,19 +177,17 @@
             this.panel3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel3.Controls.Add(this.ptbava);
-            this.panel3.Location = new System.Drawing.Point(159, 58);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel3.Location = new System.Drawing.Point(106, 37);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(232, 283);
+            this.panel3.Size = new System.Drawing.Size(155, 182);
             this.panel3.TabIndex = 3;
             // 
             // ptbava
             // 
             this.ptbava.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ptbava.Location = new System.Drawing.Point(4, 5);
-            this.ptbava.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ptbava.Location = new System.Drawing.Point(3, 3);
             this.ptbava.Name = "ptbava";
-            this.ptbava.Size = new System.Drawing.Size(220, 272);
+            this.ptbava.Size = new System.Drawing.Size(147, 174);
             this.ptbava.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ptbava.TabIndex = 0;
             this.ptbava.TabStop = false;
@@ -214,10 +198,9 @@
             this.btlogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btlogout.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold);
             this.btlogout.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btlogout.Location = new System.Drawing.Point(148, 733);
-            this.btlogout.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btlogout.Location = new System.Drawing.Point(99, 469);
             this.btlogout.Name = "btlogout";
-            this.btlogout.Size = new System.Drawing.Size(261, 72);
+            this.btlogout.Size = new System.Drawing.Size(174, 46);
             this.btlogout.TabIndex = 6;
             this.btlogout.Text = "Logout";
             this.btlogout.UseVisualStyleBackColor = false;
@@ -235,39 +218,26 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.tbfn_home);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 60);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Location = new System.Drawing.Point(0, 39);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(554, 888);
+            this.panel1.Size = new System.Drawing.Size(370, 568);
             this.panel1.TabIndex = 1;
-            // 
-            // membershipToolStripMenuItem
-            // 
-            this.membershipToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 13.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.membershipToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.membershipToolStripMenuItem.Name = "membershipToolStripMenuItem";
-            this.membershipToolStripMenuItem.Size = new System.Drawing.Size(248, 54);
-            this.membershipToolStripMenuItem.Text = "Membership";
-            this.membershipToolStripMenuItem.Click += new System.EventHandler(this.membershipToolStripMenuItem_Click);
             // 
             // FormHome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1245, 948);
+            this.ClientSize = new System.Drawing.Size(830, 607);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FormHome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormHome";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.ptbava)).EndInit();
             this.panel1.ResumeLayout(false);
@@ -284,7 +254,6 @@
         private System.Windows.Forms.ToolStripMenuItem orderToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem accountToolStripMenuItem;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox tbfn_home;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tbun_home;
